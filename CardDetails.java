a531bc90636e840e1536faf3c5f34da15352f8e5
VinceEvane <80737863+VinceEvane@users.noreply.github.com>
2021-09-23 14:03:43 +0800

    Add files via upload

---
+++ CardDetails.java
@@ -0,0 +1,16 @@
+public class CardDetails {
+    double attack, defense;
+    String cardtype, desc;
+    public double getAttack(){
+        return attack;
+    }
+    public double getDefense(){
+        return defense;
+    }
+    public String getCardtype(){
+        return cardtype;
+    }
+    public String getDescription(){
+        return desc;
+    }
+}
